06800f7d98aa939eeef6dc6340cef244c5ec93af
kolarikm <michaelodeon@gmail.com>
2018-02-03 19:31:50 -0500

    Serving static files, API working

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json')
 const express = require('express');
+const path = require('path');
 const app = express();
-const bodyParser = require('body-parser-json');
+const bodyParser = require('body-parser');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -14,6 +15,21 @@ const router = express.Router();
 
 router.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+router.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.json({"title": "Hi Amanda"});
+    console.log("GET request received");
+});
+
+router.post('/words', (req, res) => {
+    const {name, word} = req.body;
+    db.ref(`words/${name}`).push({word});
+    console.log("POST request received");
+    res.sendStatus(201);
+})
+
+app.use('/', router);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3001, () => console.log('API listening on port 3001!'));
